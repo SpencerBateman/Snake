02375a41ac33c47415463da37222fce3337390c3
Spencer Bateman <bateman.s@husky.neu.edu>
2016-08-11 23:34:04 -0400

    full interface

--- src/View/GuiView.java
+++ src/View/GuiView.java
@@ -1,7 +1,22 @@
 package View;
 
+import java.awt.event.KeyListener;
+import java.awt.event.MouseListener;
+
 /**
  * Created by spencer on 8/11/16.
  */
 public interface GuiView {
+
+  /**
+   * Intialize rendering
+   *
+   */
+  void intialize();
+
+  void addKeyListener(KeyListener keyHandler);
+
+  void addMouseListener(MouseListener mouseHandler);
+
+  void tick
 }
